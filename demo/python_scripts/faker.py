be3467f21a52ef031a0359c021426839b254e7f6
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-02-11 09:25:27 +0100

    fix: change lorem to Faker

---
+++ demo/python_scripts/faker.py
@@ -0,0 +1,6 @@
+from faker import Faker
+
+if __name__ == '__main__':
+    fake = Faker()
+    print(fake.name())
+    exit(0)
--- demo/python_scripts/lorem.py
+++
@@ -1,5 +0,0 @@
-import lorem
-
-if __name__ == '__main__':
-    print(lorem.sentence())
-    exit(0)
